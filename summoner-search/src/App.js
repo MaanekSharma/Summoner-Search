6ca576a6f09d5f1a2c090b56664d6cb715bb9298
msharma9d <maaneksharma@gmail.com>
2022-01-10 18:04:45 -0500

    Update App.js
    Fixed API KEY issue, able to return summoner profile picture and level

--- summoner-search/src/App.js
+++ summoner-search/src/App.js
@@ -8,22 +8,27 @@ function App() {
   const [searchText, setSearchText] = useState("");
   console.log(searchText);
   
-  const API_KEY = "RGAPI-df44218f-328f-466c-be90-95bc4631c3de";
+  const API_KEY = "";//user must add own Riot Games API Key for security reasons
+
+  const [playerData, setPlayerData] = useState({});
 
   function summonerSearch(event){
     //setting up API call
-    var APICallString = "http://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/"+searchText+"?API_KEY="+ API_KEY;
+    var APICallString = "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/"+searchText+"?api_key="+ API_KEY;
     
     //handling API call
     axios.get(APICallString).then(function(response){
       //success
-      console.log(response);
+      console.log(response.data);
+      setPlayerData(response.data);
     }).catch(function(error){
       //error handling
       console.log(error);
     })
   }
   
+  console.log(playerData);
+
   return (
     <div className="App">
       <div className='container'>
@@ -31,6 +36,16 @@ function App() {
         <input type="text" onChange={ e => setSearchText(e.target.value)}></input>
         <button onClick = {e => summonerSearch(e)} >Search</button>
       </div>
+
+      {JSON.stringify(playerData) != '{}' ? 
+      <>
+        <p>{playerData.name}</p>
+        <p>Summoner Level: {playerData.summonerLevel}</p>
+        <img width="100" height="100" src={"http://ddragon.leagueoflegends.com/cdn/12.1.1/img/profileicon/" + playerData.profileIconId +".png"}></img>
+      </> 
+      : <><p>Summoner data is not available</p></>
+      }
+
     </div>
   );
 }
